be1190528e30725e4ce8198f9ce8782e2731634e
Cés ( <cesarsmore@gmail.com>
2025-02-27 01:55:10 -0600

    Complete ConversorRis2Bib
    Se termino de implementar la funcionalidad del conversor Ris a Bib. También se añade archivos para los otros archivos de prueba.

--- Practica_1/conversorRis2Bib.py
+++ Practica_1/conversorRis2Bib.py
@@ -5,6 +5,7 @@ class ConversorRis2Bib(ConversorBase):
     def convertir_Contenido(self, contenido):
         contenido = self.convertirAutor(contenido)
         contenido = self.convertirTitulo(contenido)
+        contenido = self.convertirTituloLibro(contenido)
         contenido = self.convertirFecha(contenido)
         contenido = self.convertirVolumen(contenido)
         contenido = self.convertirNumeroError(contenido)
@@ -19,8 +20,9 @@ class ConversorRis2Bib(ConversorBase):
         contenido = self.convertirISBNorISSN(contenido)
         contenido = self.convertirDireccion(contenido)
         contenido = self.convertirAbstract(contenido)
-        contenido = self.convertirID(contenido)
+        #contenido = self.convertirID(contenido)
         contenido = self.convertirTipoReferencia(contenido)
+        contenido = self.agregarLlaveFinArchivo(contenido)
         
         return contenido
     
@@ -37,30 +39,138 @@ class ConversorRis2Bib(ConversorBase):
         return contenido
     
     def convertirTitulo(self, contenido):           # También servira para booktitle
+        regexTitulo = re.compile(r'\bTI\b\s*-\s*(.+)')
+        coincidencia = regexTitulo.search(contenido)
+
+        if not coincidencia:
+            return contenido
+        
+        tituloTexto = coincidencia.group(1)
+        escribirBib = "title = {" + tituloTexto + "},"
+        contenido = regexTitulo.sub(escribirBib, contenido)
+        return contenido
+    
+    def convertirTituloLibro(self, contenido):
+        regexTituloLibro = re.compile(r'\bBT\b\s*-\s*(.+)')
+        coincidencia = regexTituloLibro.search(contenido)
+
+        if not coincidencia:
+            return contenido
+        
+        tituloLibroTexto = coincidencia.group(1)
+        escribirBib = "booktitle = {" + tituloLibroTexto + "},"
+        contenido = regexTituloLibro.sub(escribirBib, contenido)
         return contenido
     
     def convertirFecha(self, contenido):
+        regexFecha = re.compile(r'\bPY\b\s*-\s*(\d{4})(?:/(\d{2}))?(?:/(\d{2}))?')        
+        coincidencia = regexFecha.search(contenido)
+
+        if not coincidencia:
+            return contenido
+        
+        conversionMes = {
+            "01": "Jan", "02": "Feb", "03": "Mar",
+            "04": "Apr", "05": "May", "06": "Jun",
+            "07": "Jul", "08": "Aug", "09": "Sep",
+            "10": "Oct", "11": "Nov", "12": "Dec"
+        }
+
+        contenidoAnio = coincidencia.group(1)
+        contenidoMes = coincidencia.group(2) if coincidencia.group(2) is not None else None
+
+        if contenidoMes is None:
+            escribirBib = "year = {" + contenidoAnio + "},"
+        else:
+            equivalenciaMes = conversionMes.get(conversionMes)
+            escribirBib = "year = {" + contenidoAnio + "},\nmonth = {" + equivalenciaMes + "},\n" + "day = {" + contenidoDia + "},"
+
+        contenido = regexFecha.sub(escribirBib, contenido, count=1)
         return contenido
     
     def convertirVolumen(self, contenido):
+        regexVolumen = re.compile(r'\bVL\b\s*-\s*(\d+)')
+        coincidencia = regexVolumen.search(contenido)
+
+        if not coincidencia:
+            return contenido
+        
+        contenidoVolumen = coincidencia.group(1)
+        escribirBib = "volume = {" + contenidoVolumen + "},"
+        contenido = regexVolumen.sub(escribirBib, contenido)
         return contenido
     
     def convertirNumeroError(self, contenido):
+        regexNumeroError = re.compile(r'\bIS\b\s*-\s*(\d+)')
+        coincidencia = regexNumeroError.search(contenido)
+        
+        if not coincidencia:
+            return contenido
+        
+        contenidoNumeroError = coincidencia.group(1)
+        escribirBib = "number = {" + contenidoNumeroError + "},"
+        contenido = regexNumeroError.sub(escribirBib, contenido)
         return contenido
     
     def convertirPaginas(self, contenido):
+        regexPagInicio = re.compile(r'\bSP\b\s*-\s*(\d+)')
+        regexPagFin = re.compile(r'\bEP\b\s*-\s*(\d+)\s*')
+        
+        pagInicio = regexPagInicio.search(contenido)
+        pagFin = regexPagFin.search(contenido)
+        
+        if not pagInicio or not pagFin:
+            return contenido
+        
+        paginas = f"pages = {{{pagInicio.group(1)}--{pagFin.group(1)}}},"
+        contenido = regexPagInicio.sub(paginas, contenido)
+        contenido = regexPagFin.sub("", contenido, count=1)
+        
         return contenido
     
     def convertirDOI(self, contenido):
+        regexDOI = re.compile(r'\bDO\b\s*-\s*(10.[\d]+\/[\S]*)\s*')
+        coincidencia = regexDOI.search(contenido)
+
+        if not coincidencia:
+            return contenido
+        
+        contenidoDOI = coincidencia.group(1)
+        escribirBib = "doi = {" + contenidoDOI + "},"
+        contenido = regexDOI.sub(escribirBib, contenido)
         return contenido
     
     def convertirURL(self, contenido):
+        regexURL = re.compile(r'\bUR\b\s*-\*(https?://[^\s{}]+)\s*')
+        coincidencia = regexURL.search(contenido)
+
+        if not coincidencia: return contenido
+
+        contenidoURL = coincidencia.group(1)
+        escribirBib = "url = {" + contenidoURL + "},"
+        contenido = regexURL.sub(escribirBib, contenido)
         return contenido
     
     def convertirEditorial(self, contenido):
+        regexEditorial = re.compile(r'\bPB\b\s*-\s*(.*)')
+        coincidencia = regexEditorial.search(contenido) 
+        
+        if not coincidencia: return contenido
+
+        contenidoEditorial = coincidencia.group(1)
+        escribirBib = "publisher = {" + contenidoEditorial + "},"
+        contenido = regexEditorial.sub(escribirBib, contenido)
         return contenido
     
     def convertirJournal(self, contenido):
+        regexJournal = re.compile(r'\bJO\b\s*-\s*(.*?)')
+        coincidencia = regexJournal.search(contenido)
+
+        if not coincidencia: return contenido
+
+        contenidoJournal = coincidencia.group(1)
+        escribirBib = "journal = {" + contenidoJournal + "},"
+        contenido = regexJournal.sub(escribirBib, contenido)
         return contenido
 
     def convertirEditores(self, contenido):
@@ -77,25 +187,91 @@ class ConversorRis2Bib(ConversorBase):
         return contenido
 
     def convertirEdicion(self, contenido):
+        regexEdicion = re.compile(r'\bET\b\s*-\s*(.*)')
+        coincidencia = regexEdicion.search(contenido)
+
+        if not coincidencia: return contenido
+
+        contenidoEdicion = coincidencia.group(1)
+        escribirBib = "edition = {" + contenidoEdicion + "},"
+        contenido = regexEdicion.sub(escribirBib, contenido)
         return contenido
     
     def convertirPalabrasClave(self, contenido):
+        regexPalabrasClave = re.compile(r'\bKW\b\s*-\s*(.+)$')
+        coincidencia = regexPalabrasClave.findall(contenido)    
+
+        if not coincidencia: return contenido
+
+        escribirBib = "keywords = {" + " and ".join(coincidencia) + "},"
+        contenido = regexPalabrasClave.sub(escribirBib, contenido, count=1)
+        contenido = regexPalabrasClave.sub("", contenido)
         return contenido
     
     def convertirISBNorISSN(self, contenido):
+        """
+        
+        ISBN: 13 dígitos para libros publicados después de 2007, 10 dígitos para libros publicados antes
+	
+        ISSN: 8 dígitos, agrupados en dos grupos de 4 dígitos
+        """
+        regexISBNorISSN = re.compile(r'\bSN\b\s*-\s*([\d-]+)')
+        coincidencia = regexISBNorISSN.search(contenido)
+        
+        if not coincidencia: return contenido
+
+        tamanioISBNorISSN = len(coincidencia.group(1))
+        if tamanioISBNorISSN == 9:
+            escribirBib = "issn = {" + coincidencia.group(1) + "},"
+        elif tamanioISBNorISSN == 14 or tamanioISBNorISSN == 17:
+            escribirBib = "isbn = {" + coincidencia.group(1) + "},"
+        contenido = regexISBNorISSN.sub(escribirBib, contenido)
         return contenido
     
     def convertirDireccion(self, contenido):
+        regexDireccion = re.compile(r'\bCY\b\s*-\s*(.*)')
+        coincidencia = regexDireccion.search(contenido)
+        if not coincidencia: return contenido
+        escribirBib = "address = {" + coincidencia.group(1) + "},"
+        contenido = regexDireccion.sub(escribirBib, contenido)
         return contenido
     
     def convertirAbstract(self, contenido):
-        return contenido
-    
-    def convertirID(self, contenido):
+        regexAbstract = re.compile(r'\bAB\b\s*-\s*(.*)')
+        coincidencia = regexAbstract.search(contenido)
+        if not coincidencia: return contenido
+        escribirBib = "abstract = {" + coincidencia.group(1) + "},"
+        contenido = regexAbstract.sub(escribirBib, contenido)
         return contenido
     
     def convertirTipoReferencia(self, contenido):
+        regexID = re.compile(r'\bID\b\s*-\s*(.*)\s*')
+        regexTipoReferencia = re.compile(r'\bTY\b\s*-\s*(.*)')
+        
+        coincidencia = regexTipoReferencia.search(contenido)
+        coincidenciaID = regexID.search(contenido)
+        
+        if not coincidenciaID: return contenido
+        if not coincidencia: return contenido
+        
+        escribirBibID = coincidenciaID.group(1)
+        contenido = regexID.sub("", contenido)
+
+        contenidoTipoReferencia = coincidencia.group(1).lower()
+        
+        if contenidoTipoReferencia == "conf": contenidoTipoReferencia = "inproceedings"
+        elif contenidoTipoReferencia == "jour": contenidoTipoReferencia = "article"
+
+        escribirBib = "@" + contenidoTipoReferencia + "{" + escribirBibID + ","
+        contenido = regexTipoReferencia.sub(escribirBib, contenido)
         return contenido
+    
+    def agregarLlaveFinArchivo(self, contenido):
+        contenido = contenido.rstrip()  # Elimina espacios en blanco y saltos de línea finales
+        if not contenido.endswith("}"):  # Si no termina con }
+            contenido += "\n}"  # Agrega la llave de cierre con un salto de línea
+        return contenido
+
     
 archivoRuta = "./archivosPrueba/conference1.ris"
 conversorRis2Bib  = ConversorRis2Bib(archivoRuta)
--- Practica_1/main.py
+++ Practica_1/main.py
@@ -6,15 +6,29 @@ rutaResultado = "./archivosConvertidos"
 if not os.path.exists("archivosConvertidos"):
     os.mkdir("archivosConvertidos")
 
-archivosBib = ["./archivosPrueba/conference1.bib",
-               "./archivosPrueba/conference2.bib", 
-               "./archivosPrueba/journal1.bib",
-               "./archivosPrueba/journal2.bib"]
+archivosBib1 = ["./archivosPrueba/conference1.bib",
+                "./archivosPrueba/conference2.bib", 
+                "./archivosPrueba/journal1.bib",
+                "./archivosPrueba/journal2.bib"]
 
-archivosRis = ["./archivosPrueba/conference1.ris",
-               "./archivosPrueba/conference2.ris",
-               "./archivosPrueba/journal1.ris",
-               "./archivosPrueba/journal2.ris"]
+archivosBib2 = ["./archivosPruebas2/BibTeX/conference_test1.bib",
+                "./archivosPruebas2/BibTeX/conference_test2.bib",
+                "./archivosPruebas2/BibTeX/conference_test3.bib",
+                "./archivosPruebas2/BibTeX/journal_test1.bib",
+                "./archivosPruebas2/BibTeX/journal_test2.bib",
+                "./archivosPruebas2/BibTeX/journal_test3.bib"]
+
+archivosRis1 = ["./archivosPrueba/conference1.ris",
+                "./archivosPrueba/conference2.ris",
+                "./archivosPrueba/journal1.ris",
+                "./archivosPrueba/journal2.ris"]
+
+archivosRis2 = ["./archivosPruebas2/RIS/conference_test1.ris",
+                "./archivosPruebas2/RIS/conference_test2.ris",
+                "./archivosPruebas2/RIS/conference_test3.ris",
+                "./archivosPruebas2/RIS/journal_test1.ris",
+                "./archivosPruebas2/RIS/journal_test2.ris",
+                "./archivosPruebas2/RIS/journal_test3.ris"] 
 while True:
 
     print("Convertir de RIS a BibTeX")
@@ -24,13 +38,13 @@ while True:
     opcion = input("Opcion:\t")
 
     if opcion == "1":
-        for indice, archivoBib in enumerate(archivosBib, start=1):
+        for indice, archivoBib in enumerate(archivosBib2, start=1):
             conversorBib2Ris = ConversorBib2Ris(archivoBib)
             salida = conversorBib2Ris.procesarArchivo("Ris")
             nuevoNombre = os.path.join(rutaResultado, f"archivo{indice}.ris")
             os.rename(salida, nuevoNombre)
     elif opcion == "2":
-        for indice, archivoRis in enumerate(archivosRis, start=1):
+        for indice, archivoRis in enumerate(archivosRis2, start=1):
             conversorRis2Bib = ConversorRis2Bib(archivoRis)
             salida = conversorRis2Bib.procesarArchivo("Bib")
             nuevoNombre = os.path.join(rutaResultado, f"archivo{indice}.bib")
